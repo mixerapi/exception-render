056f51a2ec48417efecf3159383a27e0852f77e8
Chris Nizzardini <cnizzardini@gmail.com>
2026-02-15 12:37:11 -0500

    Handle cakephp 5.3 deprecations (#160)
    Updates plugin entrypoint class names across the MixerApi plugin suite to address CakePHP 5.3 deprecation warnings and aligns CI to test newer runtime/framework ranges.

--- src/ExceptionRenderPlugin.php
+++ src/ExceptionRenderPlugin.php
@@ -6,7 +6,7 @@ namespace MixerApi\ExceptionRender;
 use Cake\Core\BasePlugin;
 use Cake\Core\PluginApplicationInterface;
 
-class Plugin extends BasePlugin
+class ExceptionRenderPlugin extends BasePlugin
 {
     /**
      * Plugin name.
